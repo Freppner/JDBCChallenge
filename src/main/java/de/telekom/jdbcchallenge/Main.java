008f6ba730efb5f1cb2a117ddc89e2f8790b51ed
freppner <taubenspiel@gmail.com>
2021-05-18 12:24:01 +0200

    JDBChallenge_20210518 seadb erstellt

---
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -0,0 +1,11 @@
+package de.telekom.jdbcchallenge;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testkomentar");
+
+	}
+
+}
