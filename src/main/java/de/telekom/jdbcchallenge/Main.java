dc570eed32b5accd1daa3ddb14a4e1943200d1a0
freppner <taubenspiel@gmail.com>
2021-05-18 13:01:45 +0200

    JDBChallenge_20210518 Treiber eingebunden

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -1,11 +1,31 @@
 package de.telekom.jdbcchallenge;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testkomentar");
 
+		final String DRIVER = "com.mysql.cj.jdbc.Driver";			//Einbinden des Datenbanktreibers
+		try {
+				Class.forName(DRIVER);
+		}catch (ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+		
+			
+		final String URL ="jdbc:mysql://localhost:3306/seadb?user=seauser&password=seapass"; 
+		try { 
+			Connection connection = DriverManager.getConnection(URL);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
