ce146c405f43ef79d529c0976c42485e02c4a789
freppner <taubenspiel@gmail.com>
2021-06-02 09:48:21 +0200

    SEA_2.0_202100602 Projekt angelegt/pom.xml erstellt.

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -2,15 +2,20 @@ package de.telekom.jdbcchallenge;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Testkomentar");
 
 		final String DRIVER = "com.mysql.cj.jdbc.Driver";			//Einbinden des Datenbanktreibers
+		// final String DRIVER = "org.mariadb.jdbc.Driver";	
 		try {
 				Class.forName(DRIVER);
 		}catch (ClassNotFoundException e) {
@@ -19,13 +24,65 @@ public class Main {
 		
 			
 		final String URL ="jdbc:mysql://localhost:3306/seadb?user=seauser&password=seapass"; 
+		//final String URL ="jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass"; 
+		Connection connection;
+		
 		try { 
-			Connection connection = DriverManager.getConnection(URL);
-		} catch (SQLException e) {
+			connection = DriverManager.getConnection(URL);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return;
 		}
+		
+		
+			//Connection connection = DriverManager.getConnection(URL);
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery( "select * from personen"); 
+		
+			while(resultSet.next()) {
+			    System.out.print("ID: "+ resultSet.getInt(1)); 	// ID
+			    System.out.print("     ");
+			    System.out.print("Anrede: "+ resultSet.getString(2)); // ANREDE
+			    System.out.print("     ");
+			    System.out.print("Vorname: "+resultSet.getString(3)); // VORNAME
+			    System.out.print("     ");
+			    System.out.print("Nachname: "+resultSet.getString(4)); // NACHNAME
+			    }
+			
+			resultSet.close();
+			statement.close();
+			connection.close();
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
